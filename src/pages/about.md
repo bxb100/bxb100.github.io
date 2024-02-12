254db703f55eddd5edb7a2791d53f82406b6142b
Lemon <bxb100@outlook.com>
2024-02-12 13:24:47 +0800

    feat: change to astro-paper

---
+++ src/pages/about.md
@@ -0,0 +1,18 @@
+---
+layout: ../layouts/AboutLayout.astro
+title: "About me"
+---
+
+I'm a software engineer with a passion for building things, know a little about Java, Rust, Javascript.
+
+Live in China mainland, currently looking for a job.
+
+Feel free to contact me when you have any questions or want to chat.
+
+## Status
+
+Alive
+
+## Death plan
+
+Not yet
---
+++ src/utils/getUniqueTags.ts
@@ -0,0 +1,24 @@
+import { slugifyStr } from "./slugify";
+import type { CollectionEntry } from "astro:content";
+import postFilter from "./postFilter";
+
+interface Tag {
+  tag: string;
+  tagName: string;
+}
+
+const getUniqueTags = (posts: CollectionEntry<"blog">[]) => {
+  if (!posts) return [] as Tag[];
+  const tags: Tag[] = posts
+    .filter(postFilter)
+    .flatMap(post => post.data.tags)
+    .map(tag => ({ tag: slugifyStr(tag), tagName: tag }))
+    .filter(
+      (value, index, self) =>
+        self.findIndex(tag => tag.tag === value.tag) === index
+    )
+    .sort((tagA, tagB) => tagA.tag.localeCompare(tagB.tag));
+  return tags;
+};
+
+export default getUniqueTags;
